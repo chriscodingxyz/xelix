f986ce3057d2d0b426e925f9da5fbf4641e74c20
cherrydub <cherrydub@gmail.com>
2024-06-08 19:22:03 +0200

    initial commit, redirecting to /upload which will then go to /review after successful upload. Will be adding shadcn next

---
+++ app/upload/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function UploadPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      upload box here
+    </main>
+  );
+}
