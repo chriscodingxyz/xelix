fb5de9065babf0a228ec634bbaa5dfbb6e2fc0ef
cherrydub <cherrydub@gmail.com>
2024-06-11 00:46:32 +0200

    updating for vercel deployment issues

--- components/InvoiceTableCN.tsx
+++ components/InvoiceTableCN.tsx
@@ -376,3 +376,24 @@ function InvoiceTableCN() {
 }
 
 export default InvoiceTableCN;
+
+{
+  /* <div className="space-x-2">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div> */
+}
