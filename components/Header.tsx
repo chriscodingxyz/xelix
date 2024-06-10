d8ef6e0ee4732ae54556232009522e0d21e1dfdd
cherrydub <cherrydub@gmail.com>
2024-06-10 12:11:19 +0200

    added export approved button, this will only show if data has length > 1 and ATLEAST one approval

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,14 +7,18 @@ import { Download, FolderOutput } from "lucide-react";
 import { useJsonData } from "@/context/JsonDataContext";
 
 export default function Header() {
-  const { data, setData } = useJsonData();
+  const { data } = useJsonData();
+
+  const hasApprovedInvoices =
+    data && data.some((invoice) => invoice.status === "approved");
+
   return (
     <header className="w-full flex items-center justify-between p-2 border-b font-bold">
       <span>
         <a href="">PayWorks</a>
       </span>
-      {data && data.length > 0 && (
-        <Button variant={"linkHover2"} size={"sm"}>
+      {data && data.length > 0 && hasApprovedInvoices && (
+        <Button variant={"outline"} size={"sm"}>
           Export <FolderOutput className="p-1" /> Approved
         </Button>
       )}
