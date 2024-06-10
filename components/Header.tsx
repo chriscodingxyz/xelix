331c5262cd2f4a4ef5671169c53b55bbec51661d
cherrydub <cherrydub@gmail.com>
2024-06-10 12:38:07 +0200

    adding logic for exported approved data, will also download a file for pending data

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,13 +12,48 @@ export default function Header() {
   const hasApprovedInvoices =
     data && data.some((invoice) => invoice.status === "approved");
 
+  const approvedData =
+    data && data.filter((invoice) => invoice.status === "approved");
+
+  const approvedAmount = approvedData.reduce(
+    (total, invoice) => total + invoice.amount,
+    0
+  );
+
+  const dataToJson = () => {
+    return JSON.stringify(
+      {
+        pay_run: {
+          date: Date.now(),
+          amount: approvedAmount,
+          invoices: approvedData,
+        },
+      },
+      null,
+      2
+    );
+  };
+
+  const handleExport = () => {
+    const blob = new Blob([dataToJson()], {
+      type: "application/json;charset=utf-8",
+    });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "payrun.json";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <header className="w-full flex items-center justify-between p-2 border-b font-bold">
       <span>
         <a href="">PayWorks</a>
       </span>
       {data && data.length > 0 && hasApprovedInvoices && (
-        <Button variant={"outline"} size={"sm"}>
+        <Button onClick={handleExport} variant={"outline"} size={"sm"}>
           Export <FolderOutput className="p-1" /> Approved
         </Button>
       )}
