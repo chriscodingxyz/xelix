4815823809fd2b5bad4ec2df77b1e0504242bc06
cherrydub <cherrydub@gmail.com>
2024-06-10 13:52:50 +0200

    adding link to normal desktop view within the qrcode

--- app/qr/page.tsx
+++ app/qr/page.tsx
@@ -1,4 +1,6 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export default function QRPage() {
@@ -6,6 +8,12 @@ export default function QRPage() {
     <div className="flex-center flex-col h-[100dvh]">
       <h1>Scan to view on Mobile</h1>
       <Image src={"/payreviewqr.png"} alt="qr code" width={256} height={256} />
+      <br />
+      <p>
+        <Link href="/upload">
+          <Button>Or proceed via Desktop here</Button>
+        </Link>
+      </p>
     </div>
   );
 }
